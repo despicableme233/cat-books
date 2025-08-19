9a8b68e59cc38ccb9583b0a123cc5cc7267d012c
liurun <3168725273@qq.com>
2025-08-19 18:21:16 +0800

    构建Maven项目多模块骨架

---
+++ cat-books-auth/src/main/java/com/adorablecat/catbooks/auth/CatBooksAuthApplication.java
@@ -0,0 +1,13 @@
+package com.adorablecat.catbooks.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatBooksAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatBooksAuthApplication.class, args);
+    }
+
+}
---
+++ cat-books-auth/src/test/java/com/adorablecat/catbooks/auth/CatBooksAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adorablecat.catbooks.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatBooksAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
